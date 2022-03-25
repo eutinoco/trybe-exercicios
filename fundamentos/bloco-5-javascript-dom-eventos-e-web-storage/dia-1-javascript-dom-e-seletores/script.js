f002cde8ad4cef4b0c4603bbc9f5e7451c10fb49
Marllon Tinoco <tinocodev@gmail.com>
2022-03-25 13:39:59 -0300

    adicionado textArea

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,14 @@
+const main = document.querySelector('main')
+const section = document.querySelectorAll('section')
+console.log(section)
+
+// for (let i = 0; i < section.length; i++) {
+//     if (section[i].id !== 'pai' || section[i].id !== 'elementoOndeVoceEsta' || section[i].id !== 'primeiroFilhoDoFilho') {
+//         section[i].remove()
+//     }
+// }
+
+// section[3].remove()
+console.log(section.length)
+
+console.log(section[0].id !== 'pai')
