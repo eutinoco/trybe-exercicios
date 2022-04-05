66dec2b749d5450d8f1c7071b72251415d54ab28
Marllon Tinoco <tinocodev@gmail.com>
2022-04-05 10:48:27 -0300

    Adicionada funçoes da parte 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -1,3 +1,5 @@
+// Parte 1
+
 const verificaCampo = (campo1, campo2) => {
     if (campo1 === "" || campo2 === "") {
         throw new Error('Por favor, preencha os dois campos!')
@@ -26,3 +28,59 @@ window.onload = () => {
     const button = document.getElementById('button');
     button.addEventListener('click', sum);
 }
+
+// parte 2
+
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
+    return (`Olá ${order.order.delivery.deliveryPerson} entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street} N°: ${order.address.number} AP: ${order.address.apartment}`)
+  }
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+    order.name = 'Luiz Silva'
+    order.payment.total = 50
+    const keys = Object.keys(order.order.pizza)
+    console.log(keys[0])
+    console.log(`Olá ${order.name}, o total do seu pedido de ${keys[0]}, ${keys[1]} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00`)
+  }
+  
+  orderModifier(order);
